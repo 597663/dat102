ba6017b89ab1b1e568773d768c14a5c780cb39f0
597663 <597663@stud.hvl.no>
2022-02-04 17:33:28 +0100

    asd

--- FilmArkiv/src/no/hvl/dat102/klient/Meny.java
+++ FilmArkiv/src/no/hvl/dat102/klient/Meny.java
@@ -1,6 +1,8 @@
 package no.hvl.dat102.klient;
 
 import no.hvl.dat102.adt.FilmarkivADT;
+import no.hvl.dat102.Film;
+import no.hvl.dat102.Sjanger;
 
 public class Meny { 
 	 private Tekstgrensesnitt tekstgr;  
@@ -12,8 +14,12 @@ public class Meny {
 	 } 
 	  
 	 public void start(){ 
-	  // legg inn en del forhåndsdefinerte filmer 
-	  // TODO 
+		 Film film1 = new Film(1, "The Godfather", "Francis Ford Coppola", 1972, "Paramount Pictures", Sjanger.ACTION);
+		 Film film2 = new Film(2, "The Shawshank Redemption", "Frank Darabont", 1994, "Castle Rock Entertainment", Sjanger.DRAMA );
+		 filmarkiv.leggTilFilm(film1);
+		 filmarkiv.leggTilFilm(film2);
+		 
+		 tekstgr.skrivUtStatistikk(filmarkiv);
 	 } 
 	 
 	} 
