573b152738f250ab3194fd2191d12b23edb60a20
Henning <Henning@PC01>
2022-02-09 14:19:30 +0100

    Testklasser opprettet

---
+++ MengdeU/Test/no/hvl/dat102/mengde/adt/MengdeADTTest.java
@@ -0,0 +1,14 @@
+package no.hvl.dat102.mengde.adt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MengdeADTTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ MengdeU/Test/no/hvl/dat102/mengde/tabell/TabellMengdeTest.java
@@ -0,0 +1,89 @@
+package no.hvl.dat102.mengde.tabell;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TabellMengdeTest {
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTabellMengde() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTabellMengdeInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAntall() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testErTom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLeggTil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFjernTilfeldig() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFjern() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInneholder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLeggTilAlle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUnion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSnitt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDifferens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUndermengde() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIterator() {
+		fail("Not yet implemented");
+	}
+
+}
